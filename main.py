1d4f2610a09bb889c29c885ff8fe9470490ba7a1
timo <tmakhlay2@gmail.com>
2019-06-27 12:31:45 -0700

    Made simple Restful API for app.

--- main.py
+++ main.py
@@ -2,10 +2,145 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+# Motor actions
+motor_state = "stop"
 
-@app.route("/1")
-def salvador():
-    return "Hello, 1"
+def motor(action):
+    if action is "forward":
+        # Ping arduino to move motor forward.
+        return "forward"
+    else if action is "stop":
+        # TODO: Ping arduino to stop moving the motor.
+        return "stop"
+    else if action is "backward":
+        # TODO: Ping arduino to move the motor backward.
+        return "backward"
+
+
+# Sensor actions
+sensor_state = False
+
+# Test app
+@app.route("/", methods=['GET'])
+def home():
+    return "Hello, Arduino Remote!"
+
+# App to change motor value
+@app.route('/ios/motor', methods=['POST'])
+def signal_motor():
+    # Handle error
+    if not request.json or not 'state' in request.json:
+        abort(400)
+
+    # update motor state
+    motor_state = request.json['state']
+    return jsonify({'state': motor_state}), 201
+
+# Tell arduino the motor state
+@app.route("/ios/sensor", methods=['GET'])
+def arduino():
+    return sensor_state
+
+
+# Tell arduino the motor state
+@app.route("/arduino/motor", methods=['GET'])
+def arduino():
+    return motor(motor_state) # In the function for now inn case I need to return something different.
+
+# Arduino Sensor state
+@app.route('/arduino/sensor', methods=['POST'])
+def signal_motor():
+    # Handle error
+    if not request.json or not 'state' in request.json:
+        abort(400)
+
+    # update motor state
+    sensor_state = request.json['state']
+    return jsonify({'state': sensor_state}), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+
+# # Example of RESTful API
+# tasks = [ # Sample data
+#     {
+#         'id': 1,
+#         'title': u'Buy groceries',
+#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'title': u'Learn Python',
+#         'description': u'Need to find a good Python tutorial on the web',
+#         'done': False
+#     }
+# ]
+#
+# def make_public_task(task):
+#     new_task = {}
+#     for field in task:
+#         if field == 'id':
+#             new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+#         else:
+#             new_task[field] = task[field]
+#     return new_task
+#
+# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+# def get_tasks():
+#     return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+#
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+# def get_task(task_id):
+#     """
+#     Get the user's task for id.
+#     Runtime: O(n)
+#     """
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     return jsonify({'task': task[0]})
+#
+# @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+# def create_task():
+#     if not request.json or not 'title' in request.json:
+#         abort(400)
+#     task = {
+#         'id': tasks[-1]['id'] + 1,
+#         'title': request.json['title'],
+#         'description': request.json.get('description', ""),
+#         'done': False
+#     }
+#     tasks.append(task)
+#     return jsonify({'task': task}), 201
+#
+#
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+# def update_task(task_id):
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     if not request.json:
+#         abort(400)
+#     if 'title' in request.json and type(request.json['title']) != unicode:
+#         abort(400)
+#     if 'description' in request.json and type(request.json['description']) is not unicode:
+#         abort(400)
+#     if 'done' in request.json and type(request.json['done']) is not bool:
+#         abort(400)
+#     task[0]['title'] = request.json.get('title', task[0]['title'])
+#     task[0]['description'] = request.json.get('description', task[0]['description'])
+#     task[0]['done'] = request.json.get('done', task[0]['done'])
+#     return jsonify({'task': task[0]})
+#
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+# def delete_task(task_id):
+#     task = [task for task in tasks if task['id'] == task_id]
+#     if len(task) == 0:
+#         abort(404)
+#     tasks.remove(task[0])
+#     return jsonify({'result': True})
